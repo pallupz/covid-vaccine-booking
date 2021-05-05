61bd411f8746f7190616b0ac41cb3d958d19d09e
bombardier-gif <83712877+bombardier-gif@users.noreply.github.com>
2021-05-05 22:22:47 +0530

    Update covid-vaccine-slot-booking.py

--- src/covid-vaccine-slot-booking.py
+++ src/covid-vaccine-slot-booking.py
@@ -14,7 +14,6 @@ def main():
         request_header = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
         }
-    try:
         if args.token:
             token = args.token
         else:
