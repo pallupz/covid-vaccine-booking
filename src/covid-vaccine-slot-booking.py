deea9aa1a759597ba06c4f3cba333796277b6d1b
pallupz <sendtopallus@gmail.com>
2021-05-07 00:49:39 +0530

    added random slot selection for first option

--- src/covid-vaccine-slot-booking.py
+++ src/covid-vaccine-slot-booking.py
@@ -17,7 +17,6 @@ def main():
     try:
         base_request_header = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
-            'x-api-key': '3sjOr2rmM52GzhpMHjDEE1kpQeRxwFDr4YcBEimi'
         }
 
         if args.token:
