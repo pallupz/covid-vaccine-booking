5993482ffe1338d931e0efe455ceff90e10a0f03
Suyash Srivastava <suyash.srivastava04@gmail.com>
2021-05-07 21:05:58 +0530

    suppressing stdout

--- src/utils.py
+++ src/utils.py
@@ -26,7 +26,8 @@ except ImportError:
 
         def beep(freq, duration):
             # brew install SoX --> install SOund eXchange universal sound sample translator on mac
-            os.system(f"play -n synth {duration/1000} sin {freq}")
+            os.system(
+                f"play -n synth {duration/1000} sin {freq} >/dev/null 2>&1")
     else:
 
         def beep(freq, duration):
