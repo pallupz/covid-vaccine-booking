565539c8b73f4da07af5ea8d910499b6fb577d8a
Vishal Vishwakarma <vishal@vishalvishwakarma.com>
2021-05-08 03:19:16 +0530

    Cleaned Captcha Image

--- src/captcha.py
+++ src/captcha.py
@@ -2,11 +2,25 @@ def captcha_buider(resp):
     from svglib.svglib import svg2rlg
     from reportlab.graphics import renderPM
     import PySimpleGUI as sg
+    import re
 
     with open('captcha.svg', 'w') as f:
         f.write(resp['captcha'])
 
+    imgfile = open("captcha.svg","r+")
+
+    captcha_cleaned = re.sub('(<path d=)(.*?)(fill=\"none\"/>)', '', imgfile.read())
+
+    imgfile.seek(0)
+    
+    imgfile.write(captcha_cleaned)
+    
+    imgfile.truncate()
+    
+    imgfile.close()
+    
     drawing = svg2rlg('captcha.svg')
+    
     renderPM.drawToFile(drawing, "captcha.png", fmt="PNG")
 
     layout = [  [ sg.Image('captcha.png') ],
