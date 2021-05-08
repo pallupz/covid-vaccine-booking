b1213bd7963a14d2592860a0ab9a7b7eda0fe00f
pallupz <sendtopallus@gmail.com>
2021-05-08 21:14:46 +0530

    added captcha support

--- src/captcha.py
+++ src/captcha.py
@@ -1,23 +1,24 @@
-def captcha_buider(resp):
-    from svglib.svglib import svg2rlg
-    from reportlab.graphics import renderPM
-    import PySimpleGUI as sg
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPM
+import PySimpleGUI as sg
 
+
+def captcha_builder(resp):
     with open('captcha.svg', 'w') as f:
         f.write(resp['captcha'])
 
     drawing = svg2rlg('captcha.svg')
     renderPM.drawToFile(drawing, "captcha.png", fmt="PNG")
 
-    layout = [  [ sg.Image('captcha.png') ],
-	    		[sg.Text("Enter Captcha Below")],
-                [sg.Input()],
-                [sg.Button('Submit',bind_return_key=True)] ]
+    layout = [[sg.Image('captcha.png')],
+              [sg.Text("Enter Captcha Below")],
+              [sg.Input()],
+              [sg.Button('Submit', bind_return_key=True)]]
 
-    window  = sg.Window('Enter Captcha',layout)
-    
+    window = sg.Window('Enter Captcha', layout)
+
     event, values = window.read()
-    
+
     window.close()
-    
+
     return values[1]
