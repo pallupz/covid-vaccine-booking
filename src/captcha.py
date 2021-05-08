4d8f221bb3c74a29d18bca3b1e7e9d0b476de29b
RohanBeast <83773978+RohanBeast@users.noreply.github.com>
2021-05-08 10:53:41 +0530

    Update captcha.py
    I have changed this captcha code into grey with opencv-python.

--- src/captcha.py
+++ src/captcha.py
@@ -2,6 +2,7 @@ def captcha_buider(resp):
     from svglib.svglib import svg2rlg
     from reportlab.graphics import renderPM
     from PIL import Image
+    import cv2
 
     with open('captcha.svg', 'w') as f:
         f.write(resp['captcha'])
@@ -9,5 +10,9 @@ def captcha_buider(resp):
     drawing = svg2rlg('captcha.svg')
     renderPM.drawToFile(drawing, "captcha.png", fmt="PNG")
 
-    img = Image.open('captcha.png')
-    img.show()
+    img = cv2.imread("captcha.png")
+    grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow(grey)
+    
+    if cv2.waitkey(1) == ord("q"):
+        cv2.destroyAllWindows()
