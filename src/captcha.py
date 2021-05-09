784149ba1bcd32a190a30c3c19ffec6fc9b37646
bombardier-gif <83712877+bombardier-gif@users.noreply.github.com>
2021-05-09 16:53:48 +0530

    Create captcha.py

---
+++ src/captcha.py
@@ -0,0 +1,22 @@
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPM
+import PySimpleGUI as sg
+import re
+
+
+def captcha_builder(resp):
+    with open('captcha.svg', 'w') as f:
+        f.write(re.sub('(<path d=)(.*?)(fill=\"none\"/>)', '', resp['captcha']))
+
+    drawing = svg2rlg('captcha.svg')
+    renderPM.drawToFile(drawing, "captcha.png", fmt="PNG")
+
+    layout = [[sg.Image('captcha.png')],
+              [sg.Text("Enter Captcha Below")],
+              [sg.Input()],
+              [sg.Button('Submit', bind_return_key=True)]]
+
+    window = sg.Window('Enter Captcha', layout)
+    event, values = window.read()
+    window.close()
+    return values[1]
