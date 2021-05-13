7016c3c67cc68d65bbb960a9dfcf8502bede011a
brosahay <3526930+brosahay@users.noreply.github.com>
2021-05-14 00:34:20 +0530

    Adds function to read OTP in macOS and iOS

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
+import time
 import json
+import sqlite3, pandas as pd
+from pathlib import Path
 from hashlib import sha256
 from collections import Counter
 from inputimeout import inputimeout, TimeoutOccurred
@@ -684,3 +687,30 @@ def generate_token_OTP(mobile, request_header):
         except Exception as e:
             print(str(e))
 
+def readOTPfromMacOS():
+    """
+    This function reads OTP from iMessage in macOS
+    :return OTP
+    """
+    wait_for_otp = 10
+    for i in range(wait_for_otp, 0, -1):
+        msg = f"Waiting for OTP. Reading OTP in {i} seconds.."
+        print(msg, end="\r", flush=True)
+        sys.stdout.flush()
+        time.sleep(1)
+    home = str(Path.home())
+    conn = sqlite3.connect(home + "/Library/Messages/chat.db")
+    messages = pd.read_sql_query("select * from message order by ROWID desc limit 1", conn)
+    handles = pd.read_sql_query("select * from handle order by ROWID desc limit 1", conn)
+    messages.rename(columns={'ROWID': 'message_id'}, inplace=True)
+    handles.rename(columns={'id': 'phone_number','ROWID': 'handle_id'}, inplace=True)
+    imessage_df = pd.merge(messages[['text', 'handle_id', 'date', 'is_sent', 'message_id']],
+                           handles[['handle_id', 'phone_number']], on='handle_id', how='left')
+
+    for index, row in imessage_df.iterrows():
+        verification_code_text = row['text']
+        if "CoWIN" in verification_code_text:
+            verification_code_text = row['text']
+            otp_text = verification_code_text.split(' ')
+            otp = otp_text[6]
+            return otp[:6]
