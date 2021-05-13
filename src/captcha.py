8c68b59eb25e00288a276fdbf487178e130e72f9
Vasu Gamdha <40864108+vasugamdha@users.noreply.github.com>
2021-05-14 05:03:09 +0530

    Forced focus of captcha
    Added focus on the captcha window and focus on the input box too. (Refer 2nd commit)

--- src/captcha.py
+++ src/captcha.py
@@ -18,10 +18,12 @@ def captcha_builder(resp):
 
     layout = [[sg.Image('captcha.gif')],
           [sg.Text("Enter Captcha Below")],
-          [sg.Input()],
+          [sg.Input(key='input')],
           [sg.Button('Submit', bind_return_key=True)]]
 
-    window = sg.Window('Enter Captcha', layout)
+    window = sg.Window('Enter Captcha', layout, finalize=True)
+    window.TKroot.focus_force()         # focus on window
+    window.Element('inp').SetFocus()    # focus on field
     event, values = window.read()
     window.close()
     return values[1]
