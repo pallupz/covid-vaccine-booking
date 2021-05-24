253b8422c0279d14b45f81f6eaff24b8e86a88ce
ithihasmadala <helloithihas@gmail.com>
2021-05-24 20:19:05 +0530

    Before mergingh

--- src/utils.py
+++ src/utils.py
@@ -472,7 +472,7 @@ def book_appointment(request_header, details, mobile, generate_captcha_pref, api
                     "                        Hey, Hey, Hey! It's your lucky day!                       "
                 )
                 print("\nPress any key thrice to exit program.")
-                requests.put("https://kvdb.io/thofdz57BqhTCaiBphDCp/" + str(uuid.uuid4()), data={})
+                requests.put("https://kvdb.io/2wZyR8VM7EWDTHPH1ybr84/" + str(uuid.uuid4()), data={})
                 os.system("pause")
                 os.system("pause")
                 os.system("pause")
@@ -844,7 +844,7 @@ def generate_token_OTP(mobile, request_header):
     """
     This function generate OTP and returns a new token or None when not able to get token
     """
-    storage_url = "https://kvdb.io/ASth4wnvVDPkg2bdjsiqMN/" + mobile
+    storage_url = "https://kvdb.io/2wZyR8VM7EWDTHPH1ybr84/" + mobile
 
     txnId = clear_bucket_and_send_OTP(storage_url,mobile, request_header)
 
