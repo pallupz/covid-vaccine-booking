ae566b9a8f301eb485372e7c59ba3dfeba95e1ae
6mxo89sqh <84678753+6mxo89sqh@users.noreply.github.com>
2021-05-24 16:15:31 +0530

    Revert "[300] Fixes location search issue for option 1"

--- src/utils.py
+++ src/utils.py
@@ -218,7 +218,6 @@ def collect_user_details(request_header):
     else:
         # Collect vaccination center preference
         location_dtls = get_pincodes()
-        pin_code_location_dtls = location_dtls
 
     print(
         "\n================================= Additional Info =================================\n"
