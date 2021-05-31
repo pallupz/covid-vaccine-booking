731d7960da598726de1f31d96ae1ce817bb381af
Christen Dsouza <christen.dsouza@citrix.com>
2021-05-31 23:33:55 +0530

    Android app - Better actionable error messages
    Fixed #419 - Display definite actionable error messages. Fixed 'null' error message.

--- src/CoWinOTPRetriever/app/src/main/java/com/indiacowin/cowinotpretriever/VolleyErrorHelper.java
+++ src/CoWinOTPRetriever/app/src/main/java/com/indiacowin/cowinotpretriever/VolleyErrorHelper.java
@@ -1,97 +1,82 @@
 package com.indiacowin.cowinotpretriever;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.NetworkError;
 import com.android.volley.NetworkResponse;
 import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
 import com.android.volley.ServerError;
 import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.apache.http.conn.ConnectTimeoutException;
+import org.json.JSONException;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.net.ConnectException;
+import java.net.MalformedURLException;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.util.Objects;
 
 public class VolleyErrorHelper {
     /**
      * Returns appropriate message which is to be displayed to the user
      * against the specified error object.
-     *
-     * @param error
-     * @param context
-     * @return
      */
-    public static String getMessage(Object error, Context context) {
-        if (error instanceof TimeoutError) {
-            return context.getResources().getString(R.string.generic_server_down);
-        }
-        else if (isServerProblem(error)) {
-            return handleServerError(error, context);
-        }
-        else if (isNetworkProblem(error)) {
-            return context.getResources().getString(R.string.no_internet);
-        }
-        return context.getResources().getString(R.string.generic_error);
-    }
-
-    /**
-     * Determines whether the error is related to network
-     * @param error
-     * @return
-     */
-    private static boolean isNetworkProblem(Object error) {
-        return (error instanceof NetworkError) || (error instanceof NoConnectionError);
-    }
-    /**
-     * Determines whether the error is related to server
-     * @param error
-     * @return
-     */
-    private static boolean isServerProblem(Object error) {
-        return (error instanceof ServerError) || (error instanceof AuthFailureError);
-    }
-    /**
-     * Handles the server error, tries to determine whether to show a stock message or to
-     * show a message retrieved from the server.
-     *
-     * @param err
-     * @param context
-     * @return
-     */
-    private static String handleServerError(Object err, Context context) {
+    public static String getMessage(Object err, Context context) {
         VolleyError error = (VolleyError) err;
+        Throwable cause = error.getCause();
 
-        NetworkResponse response = error.networkResponse;
-
-        if (response != null) {
-            switch (response.statusCode) {
-                case 404:
-                case 422:
-                case 401:
-                    try {
-                        // server might return error like this { "error": "Some error occured" }
-                        // Use "Gson" to parse the result
-                        HashMap<String, String> result = new Gson().fromJson(new String(response.data),
-                                new TypeToken<Map<String, String>>() {
-                                }.getType());
-
-                        if (result != null && result.containsKey("error")) {
-                            return result.get("error");
-                        }
-
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    // invalid request
-                    return error.getMessage();
-
-                default:
-                    return context.getResources().getString(R.string.generic_server_down);
+        if(error instanceof NoConnectionError){
+            ConnectivityManager cm = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo activeNetwork = null;
+            if (cm != null) {
+                activeNetwork = cm.getActiveNetworkInfo();
+            }
+            if(activeNetwork != null && activeNetwork.isConnectedOrConnecting()){
+                return context.getResources().getString(R.string.error_01);
+            } else {
+                return context.getResources().getString(R.string.error_02);
             }
         }
-        return context.getResources().getString(R.string.generic_error);
+        else if (error instanceof NetworkError || Objects.requireNonNull(cause) instanceof ConnectException
+                || (Objects.requireNonNull(Objects.requireNonNull(cause).getMessage()).contains("connection"))){
+            return context.getResources().getString(R.string.error_02);
+        }
+        else if (Objects.requireNonNull(cause) instanceof MalformedURLException){
+            return context.getResources().getString(R.string.error_03);
+        }
+        else if (error instanceof ParseError || Objects.requireNonNull(cause) instanceof IllegalStateException
+                || Objects.requireNonNull(cause) instanceof JSONException
+                || Objects.requireNonNull(cause) instanceof XmlPullParserException){
+            return context.getResources().getString(R.string.error_04);
+        }
+        else if (Objects.requireNonNull(cause) instanceof OutOfMemoryError){
+            return context.getResources().getString(R.string.error_05);
+        }
+        else if (error instanceof AuthFailureError){
+            return context.getResources().getString(R.string.error_06);
+        }
+        else if (error instanceof ServerError || Objects.requireNonNull(cause) instanceof ServerError) {
+            return context.getResources().getString(R.string.error_07);
+        }
+        else if (error instanceof TimeoutError || Objects.requireNonNull(cause) instanceof SocketTimeoutException
+                || Objects.requireNonNull(cause) instanceof ConnectTimeoutException
+                || Objects.requireNonNull(cause) instanceof SocketException
+                || (Objects.requireNonNull(
+                        Objects.requireNonNull(cause).getMessage()).contains(context.getResources().getString(R.string.error_08)))) {
+            return context.getResources().getString(R.string.error_08);
+        }
+        else {
+            NetworkResponse response = error.networkResponse;
+            if (response != null) return context.getResources().getString(R.string.error_09, response.statusCode);
+            return context.getResources().getString(R.string.error_10);
+        }
     }
 }
