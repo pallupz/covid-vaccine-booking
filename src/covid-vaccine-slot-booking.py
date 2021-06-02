490abc3fb31b424a1b450f68723fd57cf2a7caeb
Rooniks <me.rooniks@gmail.com>
2021-06-02 17:20:59 +0530

    Making private buckets mandatory

--- src/covid-vaccine-slot-booking.py
+++ src/covid-vaccine-slot-booking.py
@@ -45,10 +45,11 @@ def main():
             filename = filename + mobile + ".json"
             otp_pref = input("\nDo you want to enter OTP manually, instead of auto-read? \nRemember selecting n would require some setup described in README (y/n Default n): ")
             otp_pref = otp_pref if otp_pref else "n"
-            kvdb_bucket = input("If you have your personal kvdb bucket, please enter it here. Else just press enter: ")
+            kvdb_bucket = input("Please refer KVDB setup in ReadMe to setup your own KVDB bucket. Please enter your KVDB bucket value here: ")
             if not kvdb_bucket:
-                kvdb_bucket = "8o67Wac93Ha4NTMvQ3f6Me"
-            print("### Note ###\nPlease make sure the configured URL on your phone is: " + "https://kvdb.io/" + kvdb_bucket + "/" + mobile)
+                print("Sorry, having your private KVDB bucket is mandatory. Please refer ReadMe and create your own private KVBD bucket.")
+                sys.exit()
+            print("\n### Note ### Please make sure the URL configured in the IFTTT/Shortcuts app on your phone is: " + "https://kvdb.io/" + kvdb_bucket + "/" + mobile + "\n")
             while token is None:
                 if otp_pref=="n":
                     try:
